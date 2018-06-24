f0bcd61d6960bd028847384cf14ef9a0ffd6e756
Sergei Polishsuk <sepolish@student.jyu.fi>
2018-06-24 14:28:51 +0300

    tehty ohjelmointitehtava 2

--- ohjelmointiTehtavat/teht02/Program.cs
+++ ohjelmointiTehtavat/teht02/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
 
 namespace teht02
 {
@@ -6,7 +13,41 @@ namespace teht02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Socket Palvelin = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            
+            // IPEndPoint iep = new IPEndPoint(IPAddress.Any, 25000); // kukatahansa
+            IPEndPoint iep = new IPEndPoint(IPAddress.Loopback, 25000); // vain oma kone
+            Palvelin.Bind(iep);
+
+            //kunnellaan asiakkaita
+            Palvelin.Listen(5); // jonossa saa olla
+
+            // while(true) {
+                // koodi pysahtyy, palvelin odottaa yhteytta, palautaa asiakkaan socketti
+                Socket Asiakas = Palvelin.Accept(); 
+
+                IPEndPoint iap = (IPEndPoint)Asiakas.RemoteEndPoint;
+
+                System.Console.WriteLine("Yhteys osoitteesta: {0}, portista {1}", iap.Address, iap.Port);
+
+                NetworkStream ns = new NetworkStream(Asiakas);
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                String rec = sr.ReadLine();
+
+                sw.WriteLine("Sergein Palvelin;" + rec);
+                sw.Flush(); // laittaa datan eteenpain
+                Asiakas.Close();
+            // }
+
+
+            // Console.ReadKey();
+            Palvelin.Close();
+
         }
     }
 }
